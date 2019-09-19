aa43742187c6a1090f6df7286af1296923c3fcf1
Mirko <mirkopalancaji@gmail.com>
2019-09-19 15:26:45 +1000

    Adding all blog code for gatsby setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: ``,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Mirko Palancaji`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdowns`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,7 +20,9 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
---
+++ src/markdown-pages/hello-world.md
@@ -0,0 +1,22 @@
+---
+title: "My first blog post"
+date: "9-07-2019"
+---
+
+<img alt="Green forest" src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80" width="90%" />
+
+---
+
+This is the first post made for testing and playing around.
+
+Aside from that, I want to share some of my knowledge and learnings
+with others and **_hopefully_** it will be beneficial to the readers.
+
+I really look forward to deploying this blog.
+
+
+
+
+
+
+[forest]: https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80
