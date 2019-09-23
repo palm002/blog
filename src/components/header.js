279b09dbf7222483bc05655d3e8aa653b0c985d6
Mirko <mirkopalancaji@gmail.com>
2019-09-23 13:49:19 +1000

    Added logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import icon from "../../public/icons/logo.png"
+import icon from "../../src/images/logo.png"
 
 const renderIcon = () => {
   return (
