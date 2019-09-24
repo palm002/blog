2fe06f707cba7c1e9c90924c0187b26b6be1f3ca
Mirko <mirkopalancaji@gmail.com>
2019-09-24 15:20:55 +1000

    Update actionbar colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#1f262a`,
-        theme_color: `#663399`,
+        theme_color: `#1f262a`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
